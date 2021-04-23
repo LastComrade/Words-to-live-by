35ab012a0515fa7d5bda96e9445c3ca565d2c7cf
Konark Lohat <konarklohat123456@gmail.com>
2021-04-23 19:17:07 +0530

    Documentated components

--- src/Components/AdviceCard.js
+++ src/Components/AdviceCard.js
@@ -1,14 +1,15 @@
 import "../App.css"
 import Footer from './Footer'
 
-const AdviceCard = ({advice, fetch}) => {
+// Functional component
+const AdviceCard = ({advice, fetch}) => { // Take two destructured props i.e advice (string) and fetch (function)
     return (
         <div className="app flex-col">
             <div className="card">
-                <h1 className="heading">{advice}</h1>
+                <h1 className="heading">{advice}</h1> {/* Advice is string is rendered on the initial load of this website because of componentDidMount() method and advice string is inserted into the DOM*/}
                 <button
                     className="button focus:outline-none"
-                    onClick={fetch}
+                    onClick={fetch} // fetchAdvice in app.js is invoked everytime button is clicked
                 >
                     <span>GIVE ME AN ADVICE!</span>
                 </button>
