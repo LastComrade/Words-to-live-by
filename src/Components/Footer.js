e5fe371137d49ef9fcefc95596284282ce3e75a9
Konark Lohat <52380848+LastComrade@users.noreply.github.com>
2021-12-30 09:36:48 +0530

    Update Footer.js

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -25,7 +25,7 @@ const Footer = () => {
                         </li>
                     </ul>
                     <div className="flex mx-auto text-white text-center">
-                        Konark Lohat © 2021
+                        Aman Verma © 2021
                     </div>
                 </div>
             </div>
